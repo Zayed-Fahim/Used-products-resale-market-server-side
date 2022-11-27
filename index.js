d9989ea5b2b5e9d8a8189d460a73b4551b04ddc0
Zayed Fahim <zayedfahim33.cse@gmail>
2022-11-28 00:16:02 +0600

    individual category item api created

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
-require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const uri =
-    `mongodb+srv://${process.env.ADMIN}:${process.env.PASSWORD}@cluster0.llom3t1.mongodb.net/?retryWrites=true&w=majority`;
-  console.log(uri)
+const uri = `mongodb+srv://${process.env.ADMIN}:${process.env.PASSWORD}@cluster0.llom3t1.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -18,40 +16,32 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-    const categoriesCollection = client.db('phonesDotCom').collection('categories');
-    try {
-        app.get('/categories', async (req, res) => {
-            const query = {};
-            const cursor = categoriesCollection.find(query);
-            const categories = await cursor.toArray();
-            res.send(categories)
-        })
-    }
-    catch {
-        
-    }
+  const categoriesCollection = client
+    .db("phonesDotCom")
+    .collection("categories");
+  try {
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const cursor = categoriesCollection.find(query);
+      const categories = await cursor.toArray();
+      res.send(categories);
+    });
+    app.get('/categories/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const category = await categoriesCollection.findOne(query);
+      res.send(category);
+
+    })
+  } catch {
+    
+  }
 }
-run().catch(error => console.error(error));
+run().catch((error) => console.error(error));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.get('/', (req, res) => {
-    res.send('Resale phone market server is running');
-})
+app.get("/", (req, res) => {
+  res.send("Resale phone market server is running");
+});
 app.listen(port, (req, res) => {
-    console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
